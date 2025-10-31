72d9bcf8691fe9bc18f51af07b247a4e6eeed350
Ethan Lazaro <elazaro@Kessel.com>
2025-10-31 10:24:44 -0400

    Removed test_cveassert directory and removed it command from Makefile to run cve_assert.

--- llvm-plugin/tests/test_cveassert/debug_llvm.c
+++
@@ -1,9 +0,0 @@
-#include <stdlib.h>
-
-extern int DEBUG_APP_CrashCmd(void*);
-extern int DEBUG_APP_CrashCmdUnsafe(void*);
-
-int main(void) {
-    DEBUG_APP_CrashCmd(NULL);
-    // DEBUG_APP_CrashCmdUnsafe(NULL);
-}
--- llvm-plugin/tests/test_cveassert/spatial_safety/memcpy_oob.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-#include <stdlib.h>
-
-int main() {
-	int x = 10;
-	char* string_ptr = "Hello, World";
-	char *buffer = malloc(10 * sizeof(char));
-	char *ptr = memcpy(buffer, string_ptr, strlen(string_ptr) + x);
-}
