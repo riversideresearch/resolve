7be9d125afba5afb1b2a4996a951ab519566f3b4
Ethan Lazaro <elazaro@Kessel.com>
2025-12-02 11:42:21 -0500

    regression: Added heap-allocated testcase. Testing in CI.

---
+++ llvm-plugin/tests/regression/heap_oob.c
@@ -0,0 +1,19 @@
+// RUN: RESOLVE_LABEL_CVE=vulnerabilities/heap_oob.json %clang -S -emit-llvm \
+// RUN: -fpass-plugin=%plugin \
+// RUN: %s -o - | %FileCheck %s 
+// CHECK-LABEL: define dso_local i32 @main
+// RUN: RESOLVE_LABEL_CVE=vulnerabilities/heap_oob.json %clang -fpass-plugin=%plugin \ 
+// RUN: -L%rlib -lresolve -Wl,-rpath=%rlib %s -o %t.exe
+// RUN: %t.exe; test $? -eq 3
+
+#include <stdio.h>
+int main() {
+    int *ptr = malloc(2 * sizeof(int));
+    ptr[0] = 1;
+    ptr[1] = 10;
+    ptr[2] = 5;
+
+    int x = ptr[2]; 
+    return x;
+
+}
