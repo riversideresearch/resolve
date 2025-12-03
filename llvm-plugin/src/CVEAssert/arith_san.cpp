39eee9338c534c708a62e701cb95392134dfc22e
Ethan Lazaro <elazaro@Kessel.com>
2025-12-03 08:57:14 -0500

    arith_san.cpp: Removed void_ty and ptr_ty from sanitizeDivideByZero function. I do not need these.

--- llvm-plugin/src/CVEAssert/arith_san.cpp
+++ llvm-plugin/src/CVEAssert/arith_san.cpp
@@ -140,9 +140,6 @@ void sanitizeDivideByZero(Function *F,  Vulnerability::RemediationStrategies str
   auto &Ctx = M->getContext();
   IRBuilder<> Builder(Ctx);
 
-  auto void_ty = Type::getVoidTy(Ctx);
-  auto ptr_ty = PointerType::get(Ctx, 0);
-
   switch (strategy) {
     case Vulnerability::RemediationStrategies::EXIT:
       break;
