5a78f3318a48d1976af89b304b431f2420bb760c
Alex Bagnall <abagnall@riversideresearch.org>
2025-12-05 12:25:28 -0500

    binary_heap bug fixes
    - Avoid UB in `extract` on single-element heap.
    - Use correct index arithmetic for parent and children nodes in
    `_heapify_up` and `_heapify_down`.

--- reach/include/reach/binary_heap.hpp
+++ reach/include/reach/binary_heap.hpp
@@ -31,11 +31,18 @@ class binary_heap {
 
   // Extract the minimum element from the heap.
   std::pair<K, V> extract() {
+    if (_heap.empty()) {
+      throw std::out_of_range("extract on empty heap");
+    }
     const auto root = this->_heap.front();
-    this->_heap[0] = this->_heap.back();
-    this->_heap.pop_back();
     this->_ixs.erase(root.first);
+    if (this->_heap.size() == 1) {
+      this->_heap.pop_back();
+      return root;
+    }
+    this->_heap[0] = this->_heap.back();
     this->_ixs[this->_heap[0].first] = 0;
+    this->_heap.pop_back();
     this->_heapify_down(0);
     return root;
   }
@@ -69,7 +76,7 @@ class binary_heap {
   // Heapify up at index [i].
   void _heapify_up(size_t i) {
     if (i > 0) {
-      size_t parent_i = i / 2;
+      size_t parent_i = (i - 1) / 2;
       if (this->_heap[i].second < this->_heap[parent_i].second) {
         this->_swap(i, parent_i);
         this->_heapify_up(parent_i);
@@ -79,8 +86,8 @@ class binary_heap {
 
   // Heapify down at index [i].
   void _heapify_down(size_t i) {
-    size_t left_i = 2 * i;
-    size_t right_i = 2 * i + 1;
+    size_t left_i = 2 * i + 1;
+    size_t right_i = 2 * i + 2;
 
     size_t smallest = i;
     if (left_i < this->_heap.size() &&
