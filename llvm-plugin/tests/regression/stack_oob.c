d849293cab6e238a67d5e919598330a69f0aef4d
Jackson Smith <rsmith@riversideresearch.org>
2025-12-11 17:40:39 -0500

    Strict stack test

--- llvm-plugin/tests/regression/stack_oob.c
+++ llvm-plugin/tests/regression/stack_oob.c
@@ -7,30 +7,33 @@
 // RUN: -fpass-plugin=%plugin \
 // RUN: %s -o - | %FileCheck %s 
 // CHECK-LABEL: define dso_local i32 @main
-// CHECK: call void @resolve_stack_obj
-// RUN: RESOLVE_LABEL_CVE=vulnerabilities/stack_oob.json %clang -fpass-plugin=%plugin \ 
-// RUN: -L%rlib -lresolve -Wl,-rpath=%rlib %s -o %t.exe
-// RUN: %t.exe; test $? -eq 3
+// CHECK-LABEL: call void @resolve_stack_obj
 // Test that that unremediated case crashes
 // RUN: %clang -fpass-plugin=%plugin %s -o %t.exe 
-// RUN: %t.exe 200; EXIT_CODE=$?; \
+// RUN: %t.exe -2; EXIT_CODE=$?; \
 // RUN: echo Unremedated exit: $EXIT_CODE; test $EXIT_CODE -ne 0
 //
 // Test that the remediation is successful
 // RUN: RESOLVE_LABEL_CVE=vulnerabilities/stack_oob.json %clang -fpass-plugin=%plugin \ 
 // RUN: -L%rlib -lresolve -Wl,-rpath=%rlib %s -o %t.exe
-// RUN: %t.exe 200; EXIT_CODE=$?; \
+// RUN: %t.exe -2; EXIT_CODE=$?; \
 // RUN: echo Remediated exit: $EXIT_CODE; test $EXIT_CODE -eq 3
+// 
+// Test that the normal behavior is preserved
+// RUN: RESOLVE_LABEL_CVE=vulnerabilities/stack_oob.json %clang -O0 -g -fpass-plugin=%plugin \ 
+// RUN: -L%rlib -lresolve -Wl,-rpath=%rlib %s -o %t.exe
+// RUN: %t.exe 2; EXIT_CODE=$?; \
+// RUN: echo Normal exit: $EXIT_CODE; test $EXIT_CODE -eq 0
 
 #include <stdio.h>
 #include <stdlib.h>
 
 int use_stack(int buffer[], int idx) {
   int i;
-  buffer[idx] = 1;
+  buffer[idx] = 42;
 
   for (i = 0; i < 10; ++i) {
-    printf("%d ", buffer[i]);
+    printf("%d \n", buffer[i]);
   }
 
   return buffer[idx];
