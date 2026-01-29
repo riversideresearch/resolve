6c99b927f562cfbe84b8d7bb6d156d412c336a3b
Ethan Lazaro <elazaro@Kessel.com>
2026-01-29 08:14:36 -0500

    remediate.rs: Adding comment to resolve_get_base_and_limit.

--- libresolve/src/remediate.rs
+++ libresolve/src/remediate.rs
@@ -64,57 +64,6 @@ pub extern "C" fn resolve_malloc(size: usize) -> *mut c_void {
 }
 
 /**
- * @brief - Function call to replace llvm 'gep' instruction
- * @input
- *  - ptr: unique pointer root
- *  - derived: pointer derived from unique root ptr
- * @return valid pointer within bounds of allocation or
- * pointer 1-past limit of allocation
- */
-
-#[unsafe(no_mangle)]
-pub extern "C" fn resolve_gep(ptr: *mut c_void, derived: *mut c_void) -> *mut c_void {
-    let base = ptr as Vaddr;
-    let derived = derived as Vaddr;
-
-    let sobj_table = ALIVE_OBJ_LIST.lock();
-
-    // Look up the shadow object corresponding to this access.
-    let Some(sobj) = sobj_table.search_intersection(base) else {
-        warn!("[GEP] Cannot find ptr 0x{base:x} in shadow table");
-
-        // NOTE: Not doing this right now
-        // In theory it could catch bugs where integers are forced to pointers...
-        // But there are too many allocation we don't know about, like those in libc
-        // or the argv pointer.
-        // return 0 as *mut c_void;
-
-        // Assume unknown pointers are safe
-        return derived as *mut c_void;
-    };
-
-    // If shadow object exists then check if the access is within bounds
-    if sobj.contains(derived as Vaddr) {
-        info!(
-            "[GEP] ptr 0x{derived:x} valid for base 0x{base:x}, obj: {}@0x{:x}",
-            sobj.size(),
-            sobj.base
-        );
-        return derived as *mut c_void;
-    }
-
-    error!(
-        "[GEP] ptr 0x{derived:x} not valid for base 0x{base:x}, obj: {}@0x{:x}",
-        sobj.size(),
-        sobj.base
-    );
-
-    // Return 1-past limit of allocation @ ptr
-    sobj.past_limit() as *mut c_void
-}
-
-
-/**
  * @brief - Allocator logging interface for free
  * @input - ptr to the allocation
  * @return - none
@@ -298,61 +247,21 @@ pub extern "C" fn resolve_strndup(ptr: *mut c_char, size: usize) -> *mut c_char 
     string_ptr
 }
 
-/**
- * @brief - Returns true if pointer access is within bounds of a known allocation  
- * @input
- *  - base_ptr: address to be dereferenced    
- *  - size: size of dereference in bytes
- * @return true if base_ptr..size is totally within a valid shadow object  
- */
-#[unsafe(no_mangle)]
-pub extern "C" fn resolve_check_bounds(base_ptr: *mut c_void, size: usize) -> bool {
-    let base = base_ptr as Vaddr;
-
-    let sobj_table = ALIVE_OBJ_LIST.lock();
-
-    // Look up the shadow object corresponding to this access
-    if let Some(sobj) = sobj_table.search_intersection(base) {
-        // If shadow object exists then check if the access is within bounds
-        if sobj.contains(ShadowObject::limit(base, size)) {
-            // Access in Bounds
-            trace!(
-                "[BOUNDS] Access allowed {size}@0x{base:x} for allocation {}@0x{:x}",
-                sobj.size(),
-                sobj.base
-            );
-            return true;
-        } else {
-            error!(
-                "[BOUNDS] OOB access at {size}@0x{base:x} too big for allocation {}@0x{:x}",
-                sobj.size(),
-                sobj.base
-            );
-            return false;
-        }
-    }
-
-    // Check if this is an invalid pointer for one of the known shadow objects
-    if let Some(sobj) = sobj_table.search_invalid(base) {
-        error!(
-            "[BOUNDS] OOB access for {}@0x{:x}, invalid address computation",
-            sobj.size(),
-            sobj.base
-        );
-        return false;
-    }
-
-    // Not a tracked pointer, assume good to avoid trapping on otherwise valid pointers
-    // TODO: add a strict mode to reject here / add extra tracking.
-    true
-}
-
 #[repr(C)]
 pub struct ShadowObjBounds {
     pub base: *mut c_void,
     pub limit: *mut c_void,
 }
 
+/**
+ * @brief - Helper function that queries shadow obj list
+ *          to determine if the pointer is tracked, untracked,
+ *          or tainted 
+ * @input
+ *  - ptr: ptr to allocation 
+ * @return struct containing the base and limit of the
+ *         shadow object as pointers 
+ */
 #[unsafe(no_mangle)]
 pub extern "C" fn resolve_get_base_and_limit(ptr: *mut c_void) -> ShadowObjBounds {
     let sobj_table = ALIVE_OBJ_LIST.lock();
