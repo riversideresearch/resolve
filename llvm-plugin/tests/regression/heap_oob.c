2c8733b5585789635acabefcac597487e3ec7f66
Ethan Lazaro <elazaro@Kessel.com>
2026-01-29 08:04:21 -0500

    heap_oob.c: Updating testcases to reflect stack_oob testcases.

--- llvm-plugin/tests/regression/heap_oob.c
+++ llvm-plugin/tests/regression/heap_oob.c
@@ -14,14 +14,10 @@
 // RUN: %t.exe; test $? -eq 3
 
 // Test that the remediation is successful with optimizations
-// RUN: RESOLVE_LABEL_CVE=vulnerabilities/heap_oob.json %clang -S -emit-llvm \
-// RUN: -fpass-plugin=%plugin \
-// RUN: %s -o - | %FileCheck %s 
-// CHECK-LABEL: define dso_local i32 @main
-// CHECK: call ptr @resolve_malloc 
 // RUN: RESOLVE_LABEL_CVE=vulnerabilities/heap_oob.json %clang -O3 -fpass-plugin=%plugin \ 
 // RUN: -L%rlib -lresolve -Wl,-rpath=%rlib %s -o %t.exe
-// RUN: %t.exe; test $? -eq 3
+// RUN: %t.exe; EXIT_CODE=$?; \
+// RUN: echo Remediated exit: $EXIT_CODE; test $EXIT_CODE -eq 3
 
 #include <stdio.h>
 #include <stdlib.h>
