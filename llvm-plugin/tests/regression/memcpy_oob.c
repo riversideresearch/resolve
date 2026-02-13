d8cb5dcdab14faf5e2b7c471f8f25a9074b20cc8
Ethan Lazaro <elazaro@Kessel.com>
2026-02-13 12:48:23 -0500

    memcpy_oob.c: Updating testcase.

--- llvm-plugin/tests/regression/memcpy_oob.c
+++ llvm-plugin/tests/regression/memcpy_oob.c
@@ -1,14 +1,14 @@
 // Test that the remediation is successful 
-// RUN: RESOLVE_LABEL_CVE=vulnerabilities/heap_oob.json %clang -S -emit-llvm \
+// RUN: RESOLVE_LABEL_CVE=vulnerabilities/memcpy_oob.json %clang -S -emit-llvm \
 // RUN: -fpass-plugin=%plugin \
 // RUN: %s -o - | %FileCheck %s 
 // CHECK: call ptr @resolve_bounds_check_memcpy 
-// RUN: RESOLVE_LABEL_CVE=vulnerabilities/heap_oob.json %clang -O0 -g -fpass-plugin=%plugin \ 
+// RUN: RESOLVE_LABEL_CVE=vulnerabilities/memcpy_oob.json %clang -O0 -g -fpass-plugin=%plugin \ 
 // RUN: -L%rlib -lresolve -Wl,-rpath=%rlib %s -o %t.exe
 // RUN: %t.exe; test $? -eq 3
 
 // Test that the remediation is successful with optimizations
-// RUN: RESOLVE_LABEL_CVE=vulnerabilities/heap_oob.json %clang -O3 -fpass-plugin=%plugin \ 
+// RUN: RESOLVE_LABEL_CVE=vulnerabilities/memcpy_oob.json %clang -O3 -fpass-plugin=%plugin \ 
 // RUN: -L%rlib -lresolve -Wl,-rpath=%rlib %s -o %t.exe
 // RUN: %t.exe; EXIT_CODE=$?; \
 // RUN: echo Remediated exit: $EXIT_CODE; test $EXIT_CODE -eq 3
@@ -18,14 +18,18 @@
 #include <string.h>
 
 int main() {
-    char *buf = malloc(8);
+    char *x, *y;
+    x = malloc(8);
+    y = malloc(3);
     
-    // Source data
-    char source[16] = "AAAAAAAAAAAAAAAA";
+    for (size_t i = 0; i < 8; ++i) {
+        x[i] = (char)i;
+    }
 
-    memcpy(buf, source, 16);
+    memcpy(y, x, 5);
 
-    free(buf);
+    free(x);
+    free(y);
 
     return 0;
 }
