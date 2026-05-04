2a8e5ebf4f016dadaa078bd02bb5de2e54e8afb2
Ethan Lazaro <elazaro@Kessel.com>
2026-05-04 15:17:18 -0400

    instrument.cpp: Fixing compilation error.

--- resolve-cveassert/src/instrument.cpp
+++ resolve-cveassert/src/instrument.cpp
@@ -206,7 +206,7 @@ void instrumentAlloca(Function *F) {
     // if (PointerMayBeCaptured(alloca, true, true)) {
     // NOTE: Skip allocas that contain a single value
     Type *allocatedType = alloca->getAllocatedType();
-    if (allocatedType->isSingleValue()) {
+    if (allocatedType->isSingleValueType()) {
       continue;
     }
     handle_alloca(alloca);
