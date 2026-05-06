b72d79885a0e18106283d9a474ceea2085fa45d5
Ethan Lazaro <elazaro@Kessel.com>
2026-05-06 10:11:21 -0400

    provenance.rs: Adding metadata to track pointer provenance

---
+++ resolve-cveassert/libresolve/src/provenance.rs
@@ -0,0 +1,56 @@
+// Copyright (c) 2025 Riverside Research.
+// LGPL-3; See LICENSE.txt in the repo root for details.
+
+use crate::MutexWrap;
+use std::collections::BTreeMap;
+use std::ops::RangeInclusive;
+use std::ops::Bound::Included;
+
+pub type Vaddr = usize;
+
+#[derive(Debug, Clone)]
+pub struct Provenance {
+    pub base: Vaddr,
+    pub limit: Vaddr,
+    // generation: Vaddr,
+    // derived_from: Vaddr,
+}
+
+impl Provenance {
+    
+    pub fn bounds(&self) -> RangeInclusive<Vaddr> {
+        self.base..=self.limit
+    }
+
+    pub fn contains(&self, addr: Vaddr) -> bool {
+        self.bounds().contains(&addr)
+    }
+}
+
+pub struct MetadataTable {
+    table: BTreeMap<Vaddr, Provenance>,
+}
+
+impl MetadataTable {
+    pub const fn new() -> Self {
+        Self {
+            table: BTreeMap::new(),
+    }
+
+    pub fn add_pointer_metadata(&mut self, base: Vaddr, limit: Vaddr) {
+        let prov = Provenance 
+            base, 
+            limit,
+        };
+
+        self.table.insert(base, prov);
+    }
+
+    pub fn invalidate_at(&mut self, base: Vaddr) {
+        let _ = self.table.remove(&base);
+    }
+
+    pub fn search(&self, addr: Vaddr) -> Option<&Provenance> {
+        todo!("Implement search fn ");
+    }
+}
