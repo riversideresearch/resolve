d5830356650f080f522e0cbbba755e11293e16c2
Ethan Lazaro <elazaro@Kessel.com>
2026-05-07 10:49:03 -0400

    install-deps-ci.sh: Renaming install-deps -> install-deps-ci and updating documentation.

---
+++ scripts/install-deps-ci.sh
@@ -0,0 +1,70 @@
+#!/usr/bin/env bash 
+#
+# Copyright (c) 2025 Riverside Research.
+# LGPL-3; See LICENSE.txt in the repo root for details.
+
+set -e 
+
+echo "Installing dependencies for resolve..."
+
+# Update and install commands
+UPDATE_CMD="sudo apt-get update"
+INSTALL_CMD="sudo apt-get install -y --no-install-recommends"
+
+PKGS="build-essential \
+    clang \
+    clang-format \
+    clang-tidy \
+    lldb \
+    gdb \
+    lld \
+    cmake \
+    ninja-build \
+    git \
+    make \
+    wget \
+    curl \
+    zip \
+    unzip \
+    pkg-config \
+    python3-pip \
+    python3-venv \
+    python3-zstd \
+    zstd \
+    llvm-dev \
+    clang-16 \
+    llvm-16 \
+    llvm-16-dev \
+    libncurses-dev \
+    libssl-dev \
+    libz3-dev \
+    zlib1g-dev \
+    libsqlite3-dev \
+    libgoogle-perftools-dev"
+
+# Update package list
+echo "[*] Updating packages..."
+eval "$UPDATE_CMD"
+
+# Install system dependencies
+echo "[*] Installing dependencies"
+eval "$INSTALL_CMD $PKGS"
+
+# Install uv
+curl -LsSf https://astral.sh/uv/0.10.9/install.sh | env UV_INSTALL_DIR="/usr/local/bin" sh
+
+# Install python packages
+uvx pipx install --global wllvm lit
+
+# Install rust
+if ! command -v rustc >/dev/null 2>&1; then
+    echo "[*] Installing Rust via rustup..."
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --default-toolchain nightly -y  
+    source "$HOME/.cargo/env"
+else 
+    echo "[*] Rust is already installed."
+    rustc --version 
+    cargo --version
+fi 
+
+echo " All dependencies installed successfully."
