f368462307c69cc7d8c8bed4dbd9cb481d558464
Ethan Lazaro <elazaro@Kessel.com>
2026-06-18 15:46:03 -0400

    remediate.rs: Try using the mi_is_in_heap_region function to check if the pointer is within a mimalloc-owned mem region.

--- resolve-cveassert/libresolve/src/remediate.rs
+++ resolve-cveassert/libresolve/src/remediate.rs
@@ -40,10 +40,12 @@ unsafe extern "C" {
     fn mi_new(size: usize) -> *mut c_void;
     fn mi_delete(ptr: *mut c_void);
 
+    fn mi_is_in_heap_region(ptr: *mut c_void) -> bool;
     fn mi_resolve_ptr(ptr: *mut c_void) -> BoundsInfo;
     fn mi_is_heap_owned(ptr: *mut c_void) -> bool;
     fn __vasprintf(strp: *mut *mut c_char, fmt: *const c_char, args: VaList<'_>) -> c_int;
     fn resolve_return_address(level: c_uint) -> *mut c_void;
+    
 }
 
 /**
