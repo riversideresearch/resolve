49152f16a3051b07d6263231581efb20f4ce2c2b
Ryan Zmuda <ryanzmuda@gmail.com>
2026-07-01 10:19:23 -0400

    remediation example

--- examples/remediation/src/main.c
+++ examples/remediation/src/main.c
@@ -2,7 +2,7 @@
 
 int main(int argc, char *argv[]) {
     printf("Hello, UB!\n");
-    *(int*)0x0; // guaranteed segfault
+    int r = *(int*)0x0; // guaranteed segfault
     printf("Oh, I didn't crash?\n");
     return 0;
 }
