80a27bd17e22dbd4be80cea1057a14c919b9dfc7
Ethan Lazaro <elazaro@Kessel.com>
2026-07-30 09:08:50 -0400

    CVEAssert.cpp: WIP Updating variable names to be more concise and standardizing camelCase across codebase.

--- resolve-cveassert/src/CVEAssert.cpp
+++ resolve-cveassert/src/CVEAssert.cpp
@@ -44,22 +44,22 @@ bool CVE_ASSERT_DEBUG;
 DenseMap<Function *, GlobalVariable *> SanitizerMaps;
 
 GlobalVariable *getSanitizerMap(Function *F) {
-  auto It = SanitizerMaps.find(F);
-  if (It == SanitizerMaps.end()) {
+  auto it = SanitizerMaps.find(F);
+  if (it == SanitizerMaps.end()) {
     return nullptr;
   }
-  return It->second;
+  return it->second;
 }
 
 GlobalVariable *initSanitizerMap(Function &F) {
   Module *M = F.getParent();
   LLVMContext &Ctx = M->getContext();
-  Type *i1_ty = Type::getInt1Ty(Ctx);
-  ArrayType *arr_ty = ArrayType::get(i1_ty, 6);
+  Type *boolType = Type::getInt1Ty(Ctx);
+  ArrayType *ArrayType = ArrayType::get(boolType, 6);
 
   std::string globalName = F.getName().str() + ".sanmap";
 
-  Constant *gv = M->getOrInsertGlobal(globalName, arr_ty);
+  Constant *gv = M->getOrInsertGlobal(globalName, ArrayType);
   // cast<> performs an implicit assertion for Constant -> GlobalVariable
   auto *gSanitizerMap = cast<GlobalVariable>(gv);
 
@@ -68,8 +68,8 @@ GlobalVariable *initSanitizerMap(Function &F) {
   gSanitizerMap->setMetadata("cve.noinstrument", MDNode::get(Ctx, {}));
 
   if (!gSanitizerMap->hasInitializer()) {
-    std::vector<Constant *> elems(6, ConstantInt::get(i1_ty, 1));
-    gSanitizerMap->setInitializer(ConstantArray::get(arr_ty, elems));
+    std::vector<Constant *> elems(6, ConstantInt::get(boolType, 1));
+    gSanitizerMap->setInitializer(ConstantArray::get(ArrayType, elems));
   }
   return gSanitizerMap;
 }
@@ -277,13 +277,13 @@ struct LabelCVEPass : public PassInfoMixin<LabelCVEPass> {
 
     LLVMContext &Ctx = M.getContext();
     const DataLayout &DL = M.getDataLayout();
-    auto ptr_ty = PointerType::get(Ctx, 0);
-    auto size_ty = Type::getInt64Ty(Ctx);
-    auto void_ty = Type::getVoidTy(Ctx);
+    auto ptrType = PointerType::get(Ctx, 0);
+    auto intType = Type::getInt64Ty(Ctx);
+    auto voidType = Type::getVoidTy(Ctx);
 
     FunctionCallee registerFn = M.getOrInsertFunction(
         "__resolve_register_global",
-        FunctionType::get(void_ty, {ptr_ty, size_ty}, false));
+        FunctionType::get(voidType, {ptrType, intType}, false));
 
     SmallVector<GlobalVariable *, 16> targets;
     for (GlobalVariable &G : M.globals()) {
@@ -306,7 +306,7 @@ struct LabelCVEPass : public PassInfoMixin<LabelCVEPass> {
     if (targets.empty())
       return;
 
-    Function *ctor = Function::Create(FunctionType::get(void_ty, {}, false),
+    Function *ctor = Function::Create(FunctionType::get(voidType, {}, false),
                                       GlobalValue::InternalLinkage,
                                       "__resolve_register_globals_ctor", &M);
 
@@ -315,7 +315,7 @@ struct LabelCVEPass : public PassInfoMixin<LabelCVEPass> {
     IRBuilder<> builder(BasicBlock::Create(Ctx, "entry", ctor));
     for (GlobalVariable *G : targets) {
       uint64_t size = DL.getTypeAllocSize(G->getValueType());
-      builder.CreateCall(registerFn, {G, ConstantInt::get(size_ty, size)});
+      builder.CreateCall(registerFn, {G, ConstantInt::get(intType, size)});
     }
     builder.CreateRetVoid();
 
