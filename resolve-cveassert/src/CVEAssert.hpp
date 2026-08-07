803937fe0ce006eae782470370d7aa992df32bd1
Ethan Lazaro <elazaro@Kessel.com>
2026-08-07 11:17:58 -0400

    CVEAssert.hpp: clang-format.

--- resolve-cveassert/src/CVEAssert.hpp
+++ resolve-cveassert/src/CVEAssert.hpp
@@ -8,8 +8,7 @@
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/IR/GlobalVariable.h"
 
-
-enum class SanitizerFlags: uint64_t {
+enum class SanitizerFlags : uint64_t {
   DivideByZero = 0,
   IntegerOverflow,
   NullPtr,
@@ -21,7 +20,6 @@ enum class SanitizerFlags: uint64_t {
 };
 
 constexpr auto NumSanitizerFlags = static_cast<unsigned>(SanitizerFlags::Count);
-
 
 // Set value to true to get more verbose printouts
 extern bool CVE_ASSERT_DEBUG;
