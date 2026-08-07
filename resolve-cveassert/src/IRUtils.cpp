140bce375fb16b088d8b6834564f07d03f0fd7ba
Ethan Lazaro <elazaro@Kessel.com>
2026-08-07 13:58:40 -0400

    IRUtils.cpp: Adding noreturn attribute to for __resolve_exit and __resolve_recover because they do not return to normal program execution.

--- resolve-cveassert/src/IRUtils.cpp
+++ resolve-cveassert/src/IRUtils.cpp
@@ -500,6 +500,11 @@ getOrCreateRemediationBehavior(Module *M,
     return fn;
   }
 
+  AttrBuilder FnAttrs(Ctx);
+  FnAttrs.addAttribute(Attribute::NoReturn);
+  AttributeList attrs =
+      AttributeList::get(Ctx, AttributeList::FunctionIndex, FnAttrs);
+
   BasicBlock *entryBB = BasicBlock::Create(Ctx, "entry", fn);
   IRBuilder<> builder(entryBB);
 
